1f8fc3501ca3e5f09eb0c3bdbec18757da29d79a
rhubarb414 <ctophr.dubois@gmail.com>
2023-03-10 20:12:19 -0800

    adds endpoint for /notes

--- Develop/server.js
+++ Develop/server.js
@@ -7,3 +7,12 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static("public"));
+
+// GET request to go to Notes screen
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
