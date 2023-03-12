55ea1a91954359fea21552100ec85ab27c89a0c3
rhubarb414 <ctophr.dubois@gmail.com>
2023-03-11 18:45:33 -0800

    adds get endpoint for /api/notes to render saved notes in sidebar

--- Develop/server.js
+++ Develop/server.js
@@ -13,6 +13,17 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
+// POST request to save note
+app.post("/notes", (req, res) => {
+  res.body;
+});
+
+// GET request to get note database
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+// Display PORT to terminal
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
