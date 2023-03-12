372cfe84d812a0b751ba8fb76e9ee2b28f56eb05
rhubarb414 <ctophr.dubois@gmail.com>
2023-03-11 19:36:39 -0800

    adds POST endpoint for /api/notes to save note to db

--- Develop/server.js
+++ Develop/server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 // GET request to go to Notes screen
@@ -13,14 +16,42 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-// POST request to save note
-app.post("/notes", (req, res) => {
-  res.body;
-});
-
 // GET request to get note database
 app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+// POST request to save note
+app.post("/api/notes", (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to save note`);
+
+  // Destructuring assignment for note title and text in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // create newNote with properties from the req.body
+    const newNote = {
+      title,
+      text,
+    };
+
+    // Parse current db into an array so newNote can be pushed to it
+    // and then save the new db.
+    fs.readFile(`./db/db.json`, "utf8", (err, data) => {
+      const notesArr = JSON.parse(data);
+      notesArr.push(newNote);
+      const notesString = JSON.stringify(notesArr, null, 4);
+      fs.writeFile(`./db/db.json`, notesString, (err) =>
+        err
+          ? console.error(err)
+          : console.log(
+              `Note titled '${newNote.title}' has been written to JSON file`
+            )
+      );
+    });
+  }
 });
 
 // Display PORT to terminal
--- README.md
+++ README.md
@@ -7,11 +7,11 @@ GIVEN a note-taking application
 WHEN I open the Note Taker
 THEN I am presented with a landing page with a link to a notes page
 
-WHEN I click on the link to the notes page
-THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+<!-- WHEN I click on the link to the notes page
+THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column -->
 
-WHEN I enter a new note title and the note’s text
-THEN a Save icon appears in the navigation at the top of the page
+<!-- WHEN I enter a new note title and the note’s text
+THEN a Save icon appears in the navigation at the top of the page -->
 
 WHEN I click on the Save icon
 THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
